d5848e5735a3052cdded99adf618d140b998c29a
RavalisaiEnugandula <ravali.jntuhcej@gmail.com>
2025-07-21 20:35:38 +0530

    basics

---
+++ DSA/primitives.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class primitives{
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        String name=input.nextLine();
+        int age=input.nextInt();
+         System.out.println(name);
+         System.out.println(age);
+    }
+}
