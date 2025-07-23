1771d6a4b3c3e0dd01f732409f55922bd8f290df
RavalisaiEnugandula <ravali.jntuhcej@gmail.com>
2025-07-23 18:04:51 +0530

    assignment

--- fib.java
+++ fib.java
@@ -1,16 +1,37 @@
 import java.util.Scanner;
+// public class fib{
+//     public static void main(String[] args){
+//         Scanner in=new Scanner(System.in);
+//         System.out.print("Enter a number: ");
+//         int n=in.nextInt();
+//         int a=0,b=1;
+//         System.out.print("Fibonacci series are: ");
+//         for(int i=0;i<n;i++){
+//             System.out.print(a+ ",");
+//             int next=a+b;
+//             a=b;
+//             b=next;
+//         }
+//     }
+// }
 public class fib{
     public static void main(String[] args){
         Scanner in=new Scanner(System.in);
         System.out.print("Enter a number: ");
         int n=in.nextInt();
-        int a=0,b=1;
         System.out.print("Fibonacci series are: ");
         for(int i=0;i<n;i++){
-            System.out.print(a+ ",");
-            int next=a+b;
-            a=b;
-            b=next;
+            System.out.print(fibonacci(i) + " ");
         }
+    }
+    public static int fibonacci(int n){
+        if (n==0){
+            return 0;
+        }
+        if (n==1){
+            return 1;
+        }
+        int res=fibonacci(n-1)+fibonacci(n-2);
+        return res;
     }
 }
