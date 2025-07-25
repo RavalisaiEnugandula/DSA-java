10cda494c2d791ce783d1d05e7e5c544ae35554b
RavalisaiEnugandula <ravali.jntuhcej@gmail.com>
2025-07-25 22:14:45 +0530

    modified

--- conditionsAndLoops/calc.java
+++ conditionsAndLoops/calc.java
@@ -2,39 +2,52 @@ import java.util.Scanner;
 public class calc{
     public static void main(String[] args){
         Scanner in=new Scanner(System.in);
-        int ans=0;
+        // int ans=0;
         while(true){
             System.out.print("Enter operator: ");
             char op=in.next().trim().charAt(0);
-            if(op=='+'||op=='-'||op=='*'||op=='/'||op=='%'){
+        //     if(op=='+'||op=='-'||op=='*'||op=='/'||op=='%'){
                 System.out.print("Enter two numbers: ");
                 int num1=in.nextInt();
                 int num2=in.nextInt();
-                if(op=='+'){
-                    ans=num1+num2;
-                }
-                if(op=='-'){
-                    ans=num1-num2;
-                }
-                if(op=='*'){
-                    ans=num1*num2;
-                }
-                if(op=='/'){
-                    if(num2>0){
-                        ans=num1/num2;
-                    }
-                }
-                if(op=='%'){
-                    ans=num1%num2;
-                }
-                System.out.println(ans);
-            }
-            else if(op=='x'||op=='X'){
+        //         if(op=='+'){
+        //             ans=num1+num2;
+        //         }
+        //         if(op=='-'){
+        //             ans=num1-num2;
+        //         }
+        //         if(op=='*'){
+        //             ans=num1*num2;
+        //         }
+        //         if(op=='/'){
+        //             if(num2>0){
+        //                 ans=num1/num2;
+        //             }
+        //         }
+        //         if(op=='%'){
+        //             ans=num1%num2;
+        //         }
+        //         System.out.println(ans);
+        //     }
+        //     else if(op=='x'||op=='X'){
+        //         break;
+        //     }
+        //     else{
+        //     System.out.println("Invalid input");    
+        //     }
+
+        switch (op) {
+            case '+' -> System.out.println(num1+num2);
+            case '-' -> System.out.println(num1-num2);
+            case '*' -> System.out.println(num1*num2);
+            case '/' -> System.out.println(num1/num2);
+            case '%' -> System.out.println(num1%num2);
+            default -> System.out.println("Enter a valid operator");
+
+        }
+        if(op=='x'||op=='X'){
                 break;
-            }
-            else{
-            System.out.println("Invalid input");    
-            }
+         }
         }
         
     }
